5aa8e838478abd5f4b70cc59c3b42f8a8d07e733
Luke Wainhouse <lukewainhouse@hotmail.com>
2021-10-01 14:28:38 +0100

    Collections testing

---
+++ collections.py
@@ -0,0 +1,22 @@
+
+#varOfAString = 'I like pies.'
+
+#varOfShopping = ['meat', 'veg', 'coke', 'turkey', 'bacon']
+
+#print(varOfShopping[4])
+
+#cool_cows = ["Winnie the Moo", "Moolan", "Milkshake", "Mooana"]
+#cool_sheep = ["Baaaart", "Baaaarnaby"]
+#cool_pigs = ["Chris P. Bacon", "Hamlet", "Hogwarts"]
+
+#cool_pigs.insert(0,"Babe")
+#cool_pigs.remove("Hamlet")
+
+#cool_animals = [cool_cows, cool_sheep, cool_pigs]
+
+#print(cool_animals[2])
+
+varForADictionary = {'name' : 'Leon' , 'size' : 'nearly big', 'age':'I am still young' }
+
+
+print(varForADictionary)
