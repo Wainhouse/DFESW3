60cb134c01b49e3be1b1188e9d1ddb230b7b0450
Luke Wainhouse <lukewainhouse@hotmail.com>
2021-10-01 13:48:27 +0100

    Testig beginer variables and datatypes

---
+++ mytest.py
@@ -0,0 +1,32 @@
+#variableForAnInput = input("Enter a value: ")
+
+#variableForAString="this is a string"
+#variableForAnotherString="this is another string"
+# variableForAFloat=15.2
+#variableForAnIntiger= 121
+# variableForABoolean=True
+
+# print(*)
+
+
+#firstname = input("Enter firstname here")
+#lastname = input("Enter lastname here")
+
+#print("Hello, firstname, lastname") + ("! You're learning python")
+
+#firstname = input("Please enter your first name: ")
+
+#lastname = input("Please enter your last name: ")
+
+#print("Hello " + firstname + " " + lastname)
+
+#firstname = "Luke"
+#lastname = "Wainhouse"
+
+#print("Hello" + " " + firstname + " " + lastname)
+
+number1 = float(input("Please enter the first number: "))
+number2 = float(input("Please enter the second number: "))
+answer = number1 + number2
+
+print(number1, "+", number2, "=", answer)
