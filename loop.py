54d1c62ce5ffc9cb85d3cb6a1d8f7ece20f2d493
Luke Wainhouse <lukewainhouse@hotmail.com>
2021-10-04 14:22:30 +0100

    Added failed attemptd and delected collections

---
+++ loop.py
@@ -0,0 +1,50 @@
+# inputNumVar = int(input('Shove a whole number in:'))
+# resultVar = 1
+# while inputNumVar > 0:
+
+#     resultVar = resultVar * inputNumVar
+#     inputNumVar = inputNumVar -1
+
+
+# print(resultVar)
+
+# for i in range(3):
+#     for j in range(4):
+#         print(i, "x", j, "=", i * j)
+
+#...is awesome
+# count = 0
+# name = str(input("What is your name?"))
+
+# while count < 5:
+#     print(name, "is awesome!")
+#     count += 1
+
+# Print Numbers 5-10
+# for i in range(5, 11):
+#     print(i)
+
+# for i in range(10, 21, 2):
+#     print(i)
+
+# Write a while loop which asks for the names of 5 people, and for each person prints their name followed by the text "is awesome!"
+
+from typing import Text
+
+# fail
+count = 0
+name = str(input("What is your name?"))
+
+list_of_name = [name]
+
+while count < 5:
+    print(name, "is awesome!")
+    if name == name:
+
+        name2 = name.replace(input("what is your name?"))
+    print(name2 + "is awesome!")
+    # if name == name:
+    #     str.replace(input("What is your name?"))
+    #     print(name, "is awesome!")
+
+    count += 1
