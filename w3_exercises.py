32860a26e5e71f1d4521fc9585e5e7cc1f3b8d06
Luke Wainhouse <lukewainhouse@hotmail.com>
2021-10-05 13:29:05 +0100

    no2: sum all the numbers in a list

--- w3_exercises.py
+++ w3_exercises.py
@@ -1,13 +1,24 @@
 # Write a Python function to find the Max of three numbers.
 
-def max_of_two(x, y):
-    if x > y:
-        return x
-    return y
+# def max_of_two(x, y):
+#     if x > y:
+#         return x
+#     return y
 
 
-def max_of_three(x, y, z):
-    return max_of_two(x, max_of_two(y, z))
+# def max_of_three(x, y, z):
+#     return max_of_two(x, max_of_two(y, z))
 
 
-print(max_of_three(-5, 4, 8))
+# print(max_of_three(-5, 4, 8))
+
+# Write a Python function to sum all the numbers in a list.
+
+def sum_all(nums):
+    total = 0
+    for i in nums:
+        total += i
+        return total
+
+
+print(sum((4, 6, 399, 4, 3)))
